f06534c50b05d08549e6e60b97b0c50343c6784b
Yingrui Feng <yingrui@cpe-172-100-1-100.twcny.res.rr.com>
2021-04-04 00:01:05 +0800

    copy from graphql-java tutorial

---
+++ src/test/java/me/yingrui/learning/graphql/GraphQLProviderTest.java
@@ -0,0 +1,17 @@
+package me.yingrui.learning.graphql;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class GraphQLProviderTest {
+
+    GraphQLDataFetchers graphQLDataFetchers = new GraphQLDataFetchers();
+
+    @Test
+    public void test() throws IOException {
+        GraphQLProvider graphQLProvider = new GraphQLProvider(graphQLDataFetchers);
+        graphQLProvider.init();
+    }
+
+}
